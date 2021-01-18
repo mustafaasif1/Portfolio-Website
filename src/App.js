5b93a2ed54484f4b45a54d795632d0ac0f2321fe
Mustafa Asif <mustafaasif1@hotmail.com>
2021-01-18 16:58:53 +0500

    Added tic tac toe and header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/pages/Navbar";
+import Game from "./components/tic-tac-toe/Game";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      {/* <div className="layout">
+        <Game />
+      </div> */}
       <Switch>
         <Route path="/" />
       </Switch>
--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -2,24 +2,63 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MdFingerprint } from "react-icons/md";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Button } from "../Button";
+import "./Navbar.css";
+import { IconContext } from "react-icons/lib";
 
 export default function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
   return (
     <>
-      <div className="navbar">
-        <div className="navbar-container">
-          <Link to="/" calssName="navbar-logo">
-            <MdFingerprint className="navbar-icon" />
-            MUSTAFA ASIF
-          </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            {click ? <FaTimes /> : <FaBars />}
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <div className="navbar-container">
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li calssName="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li calssName="nav-item">
+                <Link
+                  to="/services"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Services
+                </Link>
+              </li>
+              <li calssName="nav-item">
+                <Link
+                  to="/products"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Products
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+      </IconContext.Provider>
     </>
   );
 }
